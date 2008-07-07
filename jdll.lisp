913ccbf43c5fec167f61aa03b17fb3b19ac512fc
Ramarren <ramarren@gmail.com>
2008-07-07 12:59:30 +0200

    Port j-fix. This gives more systematic type conversion.
    get-object and get-extended are missing

--- jdll.lisp
+++ jdll.lisp
@@ -2,3 +2,30 @@
 
 ;;; Here go things from jdll.ijs ported more or less directly
 
+(defun j-fix (type rank shape-ptr data-ptr)
+  ;; note: in jdll.ijs d=.y,0 is for arguments to memr, len there is count
+  ;; len=. */p=. i2j memr s,0,r*4
+  ;; memr defaults to type 2(char) -> uint8
+  (let ((p (iter (for i from 0 below rank)
+		 (collect (mem-aref shape-ptr :uint32 i)))))
+   (let ((len (reduce #'* p)))
+     (flet ((memr (count type)
+	      (iter (for i from 0 below count)
+		    (collect (mem-aref data-ptr type i)))))
+      (let ((datastream
+	     (ecase type
+	       ;; boolean
+	       (1 (mapcar (curry #'= 1) (memr len :uint8)))
+	       (2 (memr len :uint8))
+	       (4 (memr len :uint32))
+	       (8 (memr len :uint64))
+	       (16 (iter (for (rp ip . nil) on (memr (* 2 len) :uint64) by #'cddr)
+			 (collect (complex rp ip))))
+	       (32 (mapcar #'get-object (memr len :pointer)))
+	       (64 (mapcar #'get-extended (memr len :pointer))))))
+	(let ((out-array (make-array p)))
+	  (iter (for i from 0)
+		(for o in datastream)
+		(setf (row-major-aref out-array i)
+		      o))))))))
+
