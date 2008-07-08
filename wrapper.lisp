a7705d6aac8f8bc4b19436a32e9ad7c71676cb74
Ramarren <ramarren@gmail.com>
2008-07-08 16:32:38 +0200

    More wrapper functions from bindings to separate file.

---
+++ wrapper.lisp
@@ -0,0 +1,143 @@
+(in-package :cffi-j)
+
+;;; Lisp wrapper for J engine.
+
+(defun init ()
+  "Initialize J engine."
+  (setf *j* (init-j)))
+
+(defun free ()
+  "Free J engine. Might not actually work."
+  (free-j *j*)
+  (setf *j* nil))
+
+(defun do (cmd)
+  "Execute J statement."
+  (do-j *j* cmd))
+
+(defun cmd (cmd)
+  "Execute J statement and return the result. Will clobber `jdat` variable."
+  (let ((do-code (do (format nil "jdat =: ~a" cmd))))
+    (if (zerop do-code)
+	(get "jdat")
+	:do-error)))
+
+(defun get-boxed (data array)
+  (declare (ignore data array))
+  :cant-read-boxed)
+
+(defun get-j (j name)
+  (with-foreign-objects ((type :int)
+			 (rank :int)
+			 (shape :pointer)
+			 (data :pointer))
+    (%get j name type rank shape data)
+    (let ((j-type (ecase (mem-ref type :long)
+		    (1 :bool)
+		    (2 :literal)
+		    (4 :integer)
+		    (8 :double)
+		    (16 :complex)
+		    (32 :boxed)
+		    (64 :extended-integer)
+		    (128 :rational)
+		    (1024 :sparse-boolean)
+		    (2048 :sparse-literal)
+		    (4096 :sparse-integer)
+		    (8192 :sparse-floating-point)
+		    (16384 :sparse-complex)
+		    (32768 :sparse-boxed)
+		    (65536 :symbol)
+		    (131072 :unicode)))
+	  (rank (mem-ref rank :int))
+	  (j-shape nil))
+      (let ((result
+	     (if (zerop rank) (if (eql j-type :boxed)
+				  (get-boxed (mem-ref data :pointer) nil)
+				  (mem-ref (mem-ref data :pointer) :int))
+		 (let ((shape (iter (for i from 0 below rank)
+				    (collect (mem-aref (mem-ref shape :pointer) :int i)))))
+		   (setf j-shape shape)
+		   (let ((out-array (make-array shape)))
+		     (ecase j-type
+		      ((:integer :double :literal :bool)
+		       (iter (for i from 0 below (reduce #'* shape))
+			     (setf (row-major-aref out-array i)
+				   (mem-aref (mem-ref data :pointer) (ecase j-type
+								       (:integer :long)
+								       (:double :double)
+								       (:literal :uint8)
+								       (:bool :uint8))
+					     i))
+			     (finally (return out-array))))
+		      (:boxed (get-boxed (mem-ref data :pointer) out-array))))))))
+	(values (ecase j-type
+		  ((:integer :double :boxed :bool) result)
+		  ((:literal :unicode) (if (length= j-shape 1)
+						  (babel:octets-to-string (coerce result '(vector (unsigned-byte 8))))
+						  result)))
+		j-type
+		rank
+		j-shape)))))
+
+(defun get (name)
+  "Get J variable `name`."
+  (get-j *j* (string name)))
+
+(defun set-scalar (j name datum)
+  (with-foreign-objects ((type :int)
+			 (data-ptr :pointer))
+    (let ((foreign-type (etypecase datum
+			  (integer :long)
+			  (float :double)
+			  (character :uint8))))
+      (setf (mem-ref type :int) (ecase foreign-type
+				  (:long 4)
+				  (:double 8)
+				  (:uint8 2)))
+      (with-foreign-objects ((f-data foreign-type))
+	(setf (mem-ref f-data foreign-type) datum)
+	(setf (mem-ref data-ptr :pointer) f-data)
+	(%set j name type 0 (null-pointer) f-data)))))
+
+(defun set-array (j name data)
+  (with-foreign-objects ((type :int)
+			 (rank :int)
+			 (shape-ptr :pointer)
+			 (data-ptr :pointer))
+    (let ((foreign-type (etypecase (aref data 0)
+			    (integer :long)
+			    (float :double)
+			    (character :uint8))))
+	(setf (mem-ref type :int) (ecase foreign-type
+				    (:long 4)
+				    (:double 8)
+				    (:uint8 2)))
+	(setf (mem-ref rank :int) (array-rank data))
+	(with-foreign-objects ((f-data foreign-type (reduce #'* (array-dimensions data)))
+			       (shape :int (array-rank data)))
+	  (iter (for i from 0 below (reduce #'* (array-dimensions data)))
+		(setf (mem-aref f-data foreign-type i) (row-major-aref data i)))
+	  (setf (mem-ref data-ptr :pointer) f-data)
+	  (iter (for s in (array-dimensions data))
+		(for i from 0)
+		(setf (mem-aref shape :int i) s))
+	  (setf (mem-ref shape-ptr :pointer) shape)
+	  (%set j name type 0 shape-ptr data-ptr)))))
+
+(defun set-j (j name data)
+  (if (not (arrayp data))
+      (set-scalar j name data)
+      (set-array j name data)))
+
+(defun set (name data)
+  "Set J variable `name` to `data`. Can be either and array or a scalar, of type integer, float, simple-char."
+  (set-j *j* (string name) data))
+
+(defmacro with-j-engine (&body body)
+  "Execute body with new j-engine. Do not use this if free does not work."
+  `(let ((*j* (init-j)))
+     (unwind-protect
+	  (progn
+	    ,@body)
+       (free-j *j*))))
