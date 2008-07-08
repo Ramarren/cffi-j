215c23e7bcde3ae30c74a0ed39caf9a78612db18
Ramarren <ramarren@gmail.com>
2008-07-08 17:24:50 +0200

    Dereference pointers to pointers

--- jdll.lisp
+++ jdll.lisp
@@ -30,26 +30,26 @@
     (let ((len (if p
 		   (reduce #'* p)
 		   1)))
-     (flet ((memr (count type)
-	      (iter (for i from 0 below count)
-		    (collect (mem-aref data-ptr type i)))))
-      (let ((datastream
-	     (ecase type
-	       ;; boolean
-	       (1 (mapcar (curry #'= 1) (memr len :uint8)))
-	       (2 (memr len :uint8))
-	       (4 (memr len :uint32))
-	       (8 (memr len :double))
-	       (16 (iter (for (rp ip . nil) on (memr (* 2 len) :double) by #'cddr)
-			 (collect (complex rp ip))))
-	       (32 (mapcar #'get-object (memr len :pointer)))
-	       (64 (mapcar #'get-extended (memr len :pointer))))))
-	(if p
-	    (let ((out-array (make-array p)))
-	      (iter (for i from 0)
-		    (for o in datastream)
-		    (setf (row-major-aref out-array i)
-			  o)
-		    (finally (return out-array))))
-	    (car datastream)))))))
+      (flet ((memr (count type)
+	       (iter (for i from 0 below count)
+		     (collect (mem-aref data-ptr type i)))))
+	(let ((datastream
+	       (ecase type
+		 ;; boolean
+		 (1 (mapcar (curry #'= 1) (memr len :uint8)))
+		 (2 (memr len :uint8))
+		 (4 (memr len :uint32))
+		 (8 (memr len :double))
+		 (16 (iter (for (rp ip . nil) on (memr (* 2 len) :double) by #'cddr)
+			   (collect (complex rp ip))))
+		 (32 (mapcar #'get-object (memr len :pointer)))
+		 (64 (mapcar #'get-extended (memr len :pointer))))))
+	  (if p
+	      (let ((out-array (make-array p)))
+		(iter (for i from 0)
+		      (for o in datastream)
+		      (setf (row-major-aref out-array i)
+			    o)
+		      (finally (return out-array))))
+	      (car datastream)))))))
 
