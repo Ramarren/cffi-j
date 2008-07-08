aa632da86e7de29d47e138b5df8ecabf1d7b8f3f
Ramarren <ramarren@gmail.com>
2008-07-08 19:42:59 +0200

    Export clear function.

--- package.lisp
+++ package.lisp
@@ -1,5 +1,5 @@
 (defpackage #:cffi-j
   (:nicknames #:j)
-  (:export #:init #:free #:do #:get #:set #:cmd #:with-j-engine)
+  (:export #:init #:free #:clear #:do #:get #:set #:cmd #:with-j-engine)
   (:shadow "DO" "GET" "SET")
   (:use #:cl #:iterate #:alexandria #:cffi))
