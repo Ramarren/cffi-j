f385ddec040db0cc11043090c146f466397e5be3
Ramarren <ramarren@gmail.com>
2008-07-08 16:15:44 +0200

    Handle scalars, add dummy get functions.

--- jdll.lisp
+++ jdll.lisp
@@ -2,13 +2,22 @@
 
 ;;; Here go things from jdll.ijs ported more or less directly
 
+(defun get-object (obj-ptr)
+  )
+
+(defun get-extended (extptr)
+  )
+
 (defun j-fix (type rank shape-ptr data-ptr)
   ;; note: in jdll.ijs d=.y,0 is for arguments to memr, len there is count
   ;; len=. */p=. i2j memr s,0,r*4
   ;; memr defaults to type 2(char) -> uint8
   (let ((p (iter (for i from 0 below rank)
 		 (collect (mem-aref shape-ptr :uint32 i)))))
-   (let ((len (reduce #'* p)))
+    ;; */ on empty list is 1 (group identity)
+    (let ((len (if p
+		   (reduce #'* p)
+		   1)))
      (flet ((memr (count type)
 	      (iter (for i from 0 below count)
 		    (collect (mem-aref data-ptr type i)))))
@@ -23,9 +32,12 @@
 			 (collect (complex rp ip))))
 	       (32 (mapcar #'get-object (memr len :pointer)))
 	       (64 (mapcar #'get-extended (memr len :pointer))))))
-	(let ((out-array (make-array p)))
-	  (iter (for i from 0)
-		(for o in datastream)
-		(setf (row-major-aref out-array i)
-		      o))))))))
+	(if p
+	    (let ((out-array (make-array p)))
+	      (iter (for i from 0)
+		    (for o in datastream)
+		    (setf (row-major-aref out-array i)
+			  o)
+		    (finally (return out-array))))
+	    (car datastream)))))))
 
