04db133112e00c8a6ba212b3d96617bff51bea51
Ramarren <ramarren@gmail.com>
2009-03-31 18:15:10 +0200

    Export conditions.

--- package.lisp
+++ package.lisp
@@ -1,5 +1,15 @@
 (defpackage #:cffi-j
   (:nicknames #:j)
-  (:export #:init #:free #:clear #:do #:get #:set #:cmd #:with-j-engine)
+  (:export #:init #:free #:clear #:do #:get #:set #:cmd #:with-j-engine
+           #:j-condition
+           #:do-error
+           #:get-error
+           #:get-name-error
+           #:get-type-error
+           #:set-error
+           #:set-invalid-name
+           #:set-non8bit-character
+           #:set-heterogeneous-array
+           #:set-invalid-type)
   (:shadow "DO" "GET" "SET")
   (:use #:cl #:iterate #:alexandria #:cffi))
